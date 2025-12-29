6c01f00d8a4c9203b3d044e48bdf163ecbd0ee40
twice2x2 <twice.fresh.2x@gmail.com>
2025-12-29 12:17:03 +0900

    Add files via upload

---
+++ 10.10/1st.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "ru");
+
+    int number;
+    cout << "Введите число: ";
+    cin >> number;
+
+
+    for (; number > 0; number /= 10) {
+        int digit = number % 10;
+        cout << digit;
+    }
+
+    cout << endl;
+
+
+
+
+
+
+    return 0;
+}
