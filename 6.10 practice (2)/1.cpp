847600504fa67bcd596e936c8d98c089dc1a2049
twice2x2 <twice.fresh.2x@gmail.com>
2026-01-28 21:24:55 +0900

    Add files via upload

---
+++ 6.10 practice (2)/1.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <windows.h>
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "ru");
+	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+
+	int text, back;
+	cout << "Выберите цвет фона (От 0 до 15): ";
+	cin >> back;
+
+	cout << "Выберите цвет текста (От 0 до 15): ";
+	cin >> text;
+
+	SetConsoleTextAttribute(hConsole, (back << 4) | text);
+
+
+
+	return 0;
+}
