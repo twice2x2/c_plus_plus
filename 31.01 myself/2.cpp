ba64fa2e40ce6355e3209d1db72fa69f28fd1e91
twice2x2 <twice.fresh.2x@gmail.com>
2026-01-31 14:08:41 +0900

    Add files via upload

---
+++ 31.01 myself/2.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    string inputFile, outputFile;
+
+    //Ввод имён файлов
+    cout << "Введите имя исходного файла: ";
+    cin >> inputFile;
+
+    cout << "Введите имя выходного файла: ";
+    cin >> outputFile;
+
+    ifstream fin(inputFile);
+    ofstream fout(outputFile);
+
+    if (!fin.is_open()) {
+        cout << "Не удалось открыть исходный файл!" << endl;
+        return 1;
+    }
+
+    if (!fout.is_open()) {
+        cout << "Не удалось создать выходной файл!" << endl;
+        return 1;
+    }
+
+    string line;
+    while (getline(fin, line)) {
+        for (char& c : line) {
+            if (c == '0')
+                c = '1';
+            else if (c == '1')
+                c = '0';
+        }
+        fout << line << endl;
+    }
+
+    fin.close();
+    fout.close();
+
+    cout << "Файл успешно обработан!" << endl;
+
+    return 0;
+}
---
+++ 31.01 myself/5.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    string filename;
+    int lines = 0;
+    string line;
+
+    //Ввод имени файла
+    cout << "Введите имя файла: ";
+    cin >> filename;
+
+    ifstream file(filename);
+    if (!file.is_open()) {
+        cout << "Не удалось открыть файл!" << endl;
+        return 1;
+    }
+
+    //Подсчёт строк
+    while (getline(file, line)) {
+        lines++;
+    }
+
+    file.close();
+
+    cout << "Количество строк в файле: " << lines << endl;
+
+    return 0;
+}
